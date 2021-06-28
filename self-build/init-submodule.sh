f72dda8dfa73d7cb4f3d30f304518757029b8c01
Cheese Allergy Hamster <shlifedev@gmail.com>
2021-06-29 06:50:03 +0900

    self-build

---
+++ self-build/init-submodule.sh
@@ -0,0 +1,5 @@
+git submodule add -b docking_inter https://github.com/cimgui/cimgui.git
+git submodule add https://github.com/cimgui/cimplot.git
+git submodule add https://github.com/cimgui/cimnodes.git
+git submodule add https://github.com/cimgui/cimguizmo.git
+git submodule update --init --recursive
